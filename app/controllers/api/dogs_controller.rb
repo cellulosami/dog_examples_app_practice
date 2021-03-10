79488b0d47e746f6b17e96dcbde64fde55c90f44
austin shields <ajshields196@gmail.com>
2021-03-09 20:15:03 -0500

    add authentication to dogs create method

--- app/controllers/api/dogs_controller.rb
+++ app/controllers/api/dogs_controller.rb
@@ -1,11 +1,16 @@
 class Api::DogsController < ApplicationController
   def create
-    @dog = Dog.new(
-      name: params[:name],
-      age: params[:age],
-      breed: params[:breed]
-    )
-    @dog.save
-    render "show.json.jb"
+    if current_user
+      @dog = Dog.new(
+        name: params[:name],
+        age: params[:age],
+        breed: params[:breed]
+      )
+      @dog.save
+      p current_user
+      render "show.json.jb"
+    else
+      render json: { message: "You must be logged in in order to, uh, 'create' new dogs." }
+    end
   end
 end
