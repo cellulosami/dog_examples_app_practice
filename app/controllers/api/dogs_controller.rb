6951f268311e297f3617ca51e82b81c2869834bf
austin shields <ajshields196@gmail.com>
2021-03-09 20:34:33 -0500

    allow user to add image when using dogs create method

--- app/controllers/api/dogs_controller.rb
+++ app/controllers/api/dogs_controller.rb
@@ -1,6 +1,7 @@
 class Api::DogsController < ApplicationController
   def create
     if current_user
+      @image = Image.new(url: params[:url])
       @dog = Dog.new(
         name: params[:name],
         age: params[:age],
@@ -8,6 +9,8 @@ class Api::DogsController < ApplicationController
         user_id: current_user.id
       )
       @dog.save
+      @image.dog_id = @dog.id
+      @image.save
       render "show.json.jb"
     else
       render json: { message: "You must be logged in in order to, uh, 'create' new dogs." }
